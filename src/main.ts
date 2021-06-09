29b18c5867e530204096bf9b16234ff1028929dc
iyasin <ibr.yasn@gmail.com>
2021-06-09 18:29:14 +0300

    add delete environment feature

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import deactivateDeployments from "./deactivateDeployments";
+import deleteDeployments from "./deleteDeployments";
+import githubClient from './githubClient';
 
 type DeploymentState =
   | "error"
@@ -15,25 +18,32 @@ async function run() {
     const context = github.context;
     const defaultUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
 
-    const token = core.getInput("token", { required: true });
     const url = core.getInput("target_url", { required: false }) || defaultUrl;
     const description = core.getInput("description", { required: false }) || "";
-    const deploymentId = core.getInput("deployment_id");
+    const deploymentId = core.getInput("deployment_id", { required: false });
+    const environment =
+    core.getInput("environment", { required: false }) || "production";
     const environmentUrl =
       core.getInput("environment_url", { required: false }) || "";
     const state = core.getInput("state") as DeploymentState;
+    const deleteEnvironment = core.getInput("delete_environment", { required: false }) === "true";
 
-    const client = new github.GitHub(token, { previews: ["flash", "ant-man"] });
 
-    await client.repos.createDeploymentStatus({
-      ...context.repo,
-      deployment_id: parseInt(deploymentId),
-      state,
-      log_url: defaultUrl,
-      target_url: url,
-      description,
-      environment_url: environmentUrl,
-    });
+    if(deploymentId) {
+      await githubClient.repos.createDeploymentStatus({
+        ...context.repo,
+        deployment_id: parseInt(deploymentId),
+        state,
+        log_url: defaultUrl,
+        target_url: url,
+        description,
+        environment_url: environmentUrl,
+      });
+    }
+    if(deleteEnvironment) {
+      await deactivateDeployments(context.repo, environment);
+      await deleteDeployments(context.repo, environment);
+    }
   } catch (error) {
     core.error(error);
     core.setFailed(error.message);
